39b241c49c6505e06eacfd7b679f94ffa8c85055
Aayush Kesarwani <kesarwani19ak@gmail.com>
2026-09-12 13:47:50 +0000

    trip_fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,15 @@
+with trips as (
+    select
+    RIDE_ID,
+    RIDEABLE_TYPE,
+    DATE(TO_TIMESTAMP(STARTED_AT)) as TRIP_DATE,
+    START_STATIO_ID AS START_STATION_ID,
+    END_STATION_ID,
+    MEMBER_CSUAL AS MEMBER_CASUAL,
+    TIMESTAMPDIFF(SECOND, TO_TIMESTAMP(STARTED_AT), TO_TIMESTAMP(ENDED_AT)) AS TRIP_DURATION_SECONDS
+    from {{ source('demo', 'bike') }}
+    where ride_id != 'ride_id'
+    limit 10
+)
+
+select * from trips
